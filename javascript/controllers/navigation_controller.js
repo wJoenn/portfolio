262d09db26c60529a581f965586def83f8978261
Louis Ramos <louisramosdev@gmail.com>
2023-04-09 10:53:53 +0200

    Changed navigation click event to scroll

--- javascript/controllers/navigation_controller.js
+++ javascript/controllers/navigation_controller.js
@@ -2,10 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navigation"
 export default class extends Controller {
-  static targets = ["link"]
+  static targets = ["link", "section"]
 
-  click(event) {
-    this.linkTargets.forEach(e => e.classList.remove("active"))
-    event.target.classList.add("active")
+  handleScroll() {
+    const coords = this.sectionTargets.map(s => Math.abs(s.getBoundingClientRect().x))
+    const index = coords.indexOf(Math.min(...coords))
+
+    if (!this.linkTargets[index].classList.contains("active")) {
+      this.linkTargets.forEach(e => e.classList.remove("active"))
+      this.linkTargets[index].classList.add("active")
+    }
   }
 }
