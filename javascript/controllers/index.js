d0434d282ad9bd52c8db5542c52b2819fa8187c9
Louis Ramos <louisramosdev@gmail.com>
2023-04-09 15:40:09 +0200

    Added aos

--- javascript/controllers/index.js
+++ javascript/controllers/index.js
@@ -1,5 +1,8 @@
 import { application } from "./application.js"
 
+import AosController from "./aos_controller"
+application.register("aos", AosController)
+
 import BlobController from "./blob_controller.js"
 application.register("blob", BlobController)
 
