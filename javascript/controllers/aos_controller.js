956027383f81780260dd165e0bbcabe8aca5fe67
Louis Ramos <louisramosdev@gmail.com>
2023-04-16 21:25:56 +0200

    Deleted aos controller

--- javascript/controllers/aos_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-import AOS from "aos"
-
-// Connects to data-controller="aos"
-export default class extends Controller {
-  connect() {
-    AOS.init()
-  }
-}
